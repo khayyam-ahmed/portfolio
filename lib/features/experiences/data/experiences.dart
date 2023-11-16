4739a103db97e39260e30182fe2bfcd692872015
Khayyam Ahmed <ahmed.khayyam012@gmail.com>
2023-11-16 21:37:24 +0500

    update experieces data

--- lib/features/experiences/data/experiences.dart
+++ lib/features/experiences/data/experiences.dart
@@ -15,7 +15,7 @@ final experiencesList = [
     job: 'Game Development and NLP Intern',
     company: 'LearnOBots',
     description:
-        "As an intern, I spearheaded the development of a concept game utilizing the Phaser framework. This web-based interactive experience was designed for children, focusing on fostering engagement through sentence-based interactions with Non-Player Characters (NPCs). I played a pivotal role in crafting the virtual world, ensuring a seamless fusion of game development and natural language processing (NLP). My responsibilities included creating custom chatbots tailored to each NPC's characteristics, with the underlying NLP model trained using Torch. This experience honed my skills in both game design and NLP.",
+        "As he Phaser framework. This web-based interactive experience was designed for children, focusing on fostering engagement through sentence-based interactions with Non-Player Characters (NPCs). I played a pivotal role in crafting the virtual world, ensuring a seamless fusion of game development and natural language processing (NLP). My responsibilities included creating custom chatbots tailored to each NPC's characteristics, with the underlying NLP model trained using Torch. This experience honed my skills in both game design and NLP.",
     isPresent: false,
     startYear: 2021,
     endYear: 2021,
