de2a7a13c69d0e9c6e0b5bd668c34a2bd1a79b4a
Khayyam Ahmed <ahmed.khayyam012@gmail.com>
2023-11-22 15:27:17 +0500

    Added Mobile Screen

---
+++ lib/features/personal_info/personal_info_mobile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/common/constants/sizes.dart';
+import 'package:portfolio/features/personal_info/data/personal_info.dart';
+import 'package:portfolio/features/personal_info/widgets/contact_bar.dart';
+import 'package:portfolio/features/personal_info/widgets/resume_button.dart';
+
+class PersonalInfoMobile extends StatelessWidget {
+  const PersonalInfoMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contacts = contactsList;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Khayyam Ahmed',
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        gapH4,
+        Text(
+          'Software Engineer', // description
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 20),
+        ),
+        gapH8,
+        Text(
+          'Flutter & Python Enthusiast', //sub description
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        gapH8,
+        // ResumeButton(resumeUrl: resumeUrl),
+        _buildResumeButton(),
+        // const Spacer(),
+        gapH8,
+        ContactBar(contacts: contacts),
+      ],
+    );
+  }
+
+  Widget _buildResumeButton() {
+    // if (resumeUrl) return const SizedBox.shrink();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36),
+      child: ResumeButton(resumeUrl: resumeUrl),
+    );
+  }
+}
--- lib/features/personal_info/personal_info_section.dart
+++ lib/features/personal_info/personal_info_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:portfolio/common/widgets/responsive.dart';
 import 'package:portfolio/features/personal_info/personal_info_desktop.dart';
+import 'package:portfolio/features/personal_info/personal_info_mobile.dart';
 import 'package:portfolio/features/personal_info/personal_info_tablet.dart';
 
 class PersonalInfoSection extends StatelessWidget {
@@ -11,6 +12,7 @@ class PersonalInfoSection extends StatelessWidget {
     return const Responsive(
       desktop: PersonalInfoDesktop(),
       tablet: PersonalInfoTablet(),
+      mobile: PersonalInfoMobile(),
     );
   }
 }
