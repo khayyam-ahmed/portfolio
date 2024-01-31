473b7e8dcfc0dec28c2bb9c92bce40e1eb468a43
Khayyam Ahmed <ahmed.khayyam012@gmail.com>
2024-01-31 14:47:43 -0500

    updated about me

--- lib/features/about/data/about.dart
+++ lib/features/about/data/about.dart
@@ -1,2 +1,2 @@
-String aboutDescription1 =
+String aboutDescription_1 =
     "Hello, I'm Khayyam. I am a versatile software engineer with a passion for programming and a keen interest in the evolving landscape of technology. Proficient in languages like Python, C++, and Java, I have a strong track record in writing effective automation scripts, including browser automation. While I'm enthusiastic about coding and technology, I approach my work with humility and a readiness to learn, always aiming to contribute meaningfully to any team I join, whether in development, programming, or security analysis.";
