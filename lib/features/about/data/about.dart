d02774ab60e9e7e418cce69a76c8ecf1dcc0f7c0
Khayyam Ahmed <ahmed.khayyam012@gmail.com>
2025-11-12 14:34:34 -0500

    updated some strings

--- lib/features/about/data/about.dart
+++ lib/features/about/data/about.dart
@@ -1,4 +1,4 @@
 // String aboutDescription_1 =
 //     "Embarking on a transformative journey in tech, I'm Khayyam Ahmed, a software engineer pivoting towards full-stack development with a passion for machine learning and a future goal of mastering computer vision.";
 String aboutDescription_1 =
-    "Hi, I'm Khayyam. An innovative full-stack developer with strong experience in software automation. Skilled in designing scalable web/mobile applications and building tools that optimize workflows. Actively seeking opportunities to contribute to innovative IT solutions.";
+    "Hello, I'm Khayyam. An innovative full-stack developer with strong experience in software automation. Skilled in designing scalable web/mobile applications and building tools that optimize workflows. Actively seeking opportunities to contribute to innovative IT solutions.";
